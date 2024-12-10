d82a5522a94b8485f31a585e1ccfb42c172d3ba2
Khurshedspi <khurshed5322@gmail.com>
2024-12-11 00:40:53 +0600

    Add: firebase env

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,23 +1,39 @@
 import Lottie from "lottie-react";
-import React from "react";
+import React, { useState } from "react";
 import registerLottieData from "../../assets/lottie/register.json"
 
 const Register = () => {
+    const [errorMessage, SetErrorMessage] = useState("");
+
+    const handleRegister = (e)=>{
+        e.preventDefault();
+
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        // password validation
+        // const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/;
+ 
+      
+    }
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
+        <div className="text-center lg:text-left w-96">
         <Lottie animationData={registerLottieData}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <h1 className="text-5xl ml-8 mt-4 font-bold">Register now!</h1>
-          <form className="card-body ">
+          <form onSubmit={handleRegister} className="card-body ">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -29,6 +45,7 @@ const Register = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
